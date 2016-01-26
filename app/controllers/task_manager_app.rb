cbc3f32de1fd6f00cb4c190d980e197b1fffecf5
Nick Weiland <nickweiland@gmail.com>
2016-01-26 16:13:18 -0700

    Add dashboard css

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,20 +1,34 @@
+require 'models/task_manager'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/' do
+    @tasks = task_manager.all
     erb :dashboard
   end
 
-  get '/tasks' do
-    @tasks = ["task1", "task2", "task3"]
-    erb :index
-  end
+  # get '/tasks' do
+  #   @tasks = task_manager.all
+  #   erb :index
+  # end
 
   get '/tasks/new' do
     erb :new
   end
 
   post '/tasks' do
-    "<p>Params: #{params} </p> <p>Task params: #{params[:task]}</p>"
+    task_manager.create(params[:task])
+    redirect '/tasks'
+  end
+
+  get '/tasks/:id' do |id|
+    @task = task_manager.find(id.to_i)
+    erb :show
+  end
+
+  def task_manager
+    database = YAML::Store.new('db/task_manager')
+    @task_manager ||= TaskManager.new(database)
   end
 end
---
+++ app/public/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".element").typed({
+        strings: ["^300Hi.^500", "Welcome to Task Manager."],
+        typeSpeed: 0
+    });
+});
