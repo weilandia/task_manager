97ea9f3f454aff31a81086b7b5642498606f2e4e
Nick Weiland <nickweiland@gmail.com>
2016-01-26 10:29:07 -0700

    Create task manager structure

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,20 @@
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2", "task3"]
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    "<p>Params: #{params} </p> <p>Task params: #{params[:task]}</p>"
+  end
+end
